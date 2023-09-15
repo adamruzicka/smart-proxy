2898158ea326976868f07c934b175cee9d0983c0
Adam Ruzicka <aruzicka@redhat.com>
2023-09-15 15:07:01 +0200

    Use patched smart_proxy_dynflow

--- bundler.d/x.local.rb
+++ bundler.d/x.local.rb
@@ -1,4 +1,4 @@
 gem 'smart_proxy_remote_execution_ssh'
-gem 'smart_proxy_dynflow', path: 'plugins/smart-proxy-dynflow'
+gem 'smart_proxy_dynflow', git: 'https://github.com/adamruzicka/smart_proxy_dynflow', branch: 'scale'
 
 gem 'pg'
